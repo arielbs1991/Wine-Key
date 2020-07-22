89152ff2561f0ce63b81e8278e717e6a51ad6856
wolfdog27 <kylenicholasanderson@gmail.com>
2020-07-22 09:58:47 -0700

    updated routes for group and restaurant

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS winekey;
--- Creates the "blogger" database --
-CREATE DATABASE winekey;
-=======
 DROP DATABASE IF EXISTS wine_key;
 CREATE DATABASE wine_key;
->>>>>>> dev
