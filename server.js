616dc97586275598d8599d3062dba88dbef622a8
Ariel <arielbs1991@gmail.com>
2020-07-22 12:50:24 -0700

    filled out index_controller.js based on author activity cms.js file

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.use(express.static("public"));
 // =============================================================
 // require("./routes/api-routes.js")(app);
 // require("./routes/html-routes.js")(app);
+require("./routes/wine-api-routes.js")(app);
+require("./routes/restaurant-api-routes.js")(app);
+// require("./routes/inventory-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
