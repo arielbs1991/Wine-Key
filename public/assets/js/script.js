2ba2216229c33835e9a7b9b869c5e59d95d87bf0
Trish Ness <trishnss@gmail.com>
2020-07-23 12:45:31 -0700

    merging in cdn

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,25 +1,7 @@
+const { endsWith } = require("sequelize/types/lib/operators")
 
+//Search "submit" button
+$(".search-wine").on("submit")
 
-
-
-
-
-
-
-
-// $(document).ready(function() {
-  
-//   $(".devour-form").on("submit", function(event) {
-//     event.preventDefault();
-  
-//     var burger_id = $(this).children(".burger_id").val();
-//     $.ajax({
-//       method: "PUT",
-//       url: "/burgers/update/" + burger_id
-//     }).then(function(data) {
-     
-//       location.reload();
-//     });
-  
-//   });
-// });
+//on click for submit button, grab value from input field then location.href=backend route+wine name (concatenate) wine name
+//back end route req.params.name, api/wine/search/:name, 
