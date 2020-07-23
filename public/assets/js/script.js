2d7536a5faefae11e8585f9a4eef1a35daf22d76
Ariel <arielbs1991@gmail.com>
2020-07-23 10:27:34 -0700

    modified routes in wine_controller file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,25 +1,19 @@
-
-
-
-
-
-
-
-
-
+//button click event to go from home page to individual restaurant page - for each restaurant
 // $(document).ready(function() {
   
-//   $(".devour-form").on("submit", function(event) {
-//     event.preventDefault();
-  
-//     var burger_id = $(this).children(".burger_id").val();
-//     $.ajax({
-//       method: "PUT",
-//       url: "/burgers/update/" + burger_id
-//     }).then(function(data) {
-     
-//       location.reload();
+//     $(".devour-form").on("submit", function(event) {
+//       event.preventDefault();
+    
+//       var burger_id = $(this).children(".burger_id").val();
+//       $.ajax({
+//         method: "PUT",
+//         url: "/burgers/update/" + burger_id
+//       }).then(function(data) {
+       
+//         location.reload();
+//       });
+    
 //     });
-  
 //   });
-// });
+
+  //submit button to grab wine name from input field and search for wine id, returning inventory information with restaurant id
